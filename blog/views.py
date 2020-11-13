a5803328ef59b33262f48c9c75b1f957c5d870d4
magicuser <flavien.chamay@protonmail.com>
2020-11-13 22:18:35 +0100

    Templates + HTML & CSS for aesthetic

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'Flavien Chamay',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : '11/11/2020'
+    },
+    {
+        'author' : 'Romain Chamay',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : '12/11/2020'
+    }
+]
 
 def homePage(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    
+    return render(request, 'blog/home.html', context)
 
 def aboutPage(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
 
