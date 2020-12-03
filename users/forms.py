d14a3b2ef6cdf21d109e50fedd190c6ff651bb2c
magicuser <flavien.chamay@protonmail.com>
2020-12-03 15:39:25 +0100

    Update user profile mechanisms and optimisation of saved images

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,19 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+#We want a form that will update our user model (email and username)
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+#We want a form that will update our profile (image)
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 #One to One relationship between a profile and a user:
 class Profile(models.Model):
@@ -11,3 +11,13 @@ class Profile(models.Model):
     #Returns a more human readable object of the profile:
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    #Overwriting save method for resizing image (triggered after our model is saved
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
